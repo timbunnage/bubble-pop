41b41b741e275bb7445f136a7648b8cb95f3c66a
Infraxion <Infraxion@users.noreply.github.com>
2023-01-07 20:13:26 +1100

    Direction based sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 4f;
-
+    public float moveSpeed = 1f;
+    public Sprite forwardSprite;
+    public Sprite rightSprite;
+    public Sprite backSprite;
+    public Sprite leftSprite;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,30 @@ public class Player : MonoBehaviour
     void Update()
     {
         // Move the player around the screen
-        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f).normalized;
         transform.position += moveSpeed * Time.deltaTime * move;
+
+        float angle = Vector2.SignedAngle(new Vector2(move.x, move.y), Vector2.up);
+        print(angle);
+
+        if (move.normalized.magnitude > 0)
+        {
+            if (angle > -45 && angle < 45)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = forwardSprite;
+            }
+            else if (angle >= 45 && angle <= 135)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = rightSprite;
+            }
+            else if (angle > 135 || angle < -135)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = backSprite;
+            }
+            else if (angle >= -135 && angle <= -45)
+            {
+                gameObject.GetComponent<SpriteRenderer>().sprite = leftSprite;
+            }
+        }
     }
 }
