d8718c6dfcad7f7f63bfba0cf45a3eb800ceb7c9
Infraxion <Infraxion@users.noreply.github.com>
2023-01-08 00:30:16 +1100

    Added grass sprite, cleanup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float moveSpeed = 1f;
+    public float moveSpeed = 4f;
     public Sprite forwardSprite;
     public Sprite rightSprite;
     public Sprite backSprite;
