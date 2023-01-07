10fce062f123483e1544034e90236cc9bc479ec9
David Tenni <davidctenni@gmail.com>
2023-01-07 16:44:02 +1100

    added colliders

--- Assets/Scripts/DeathEffect.cs
+++ Assets/Scripts/DeathEffect.cs
@@ -16,6 +16,11 @@ public class Flowe : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
         onDeath();
     }
 
