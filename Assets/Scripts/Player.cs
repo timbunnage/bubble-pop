6563946c4f9a7a7270d3a169ea800fc582f94d1a
timbunnage <tim10@bunnage.com.au>
2023-01-07 15:26:12 +1100

    Can move player with arrows or wasd

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the player around the screen
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * moveSpeed * Time.deltaTime;
+    }
+}
